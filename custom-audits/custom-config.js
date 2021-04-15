69f37bd68aee72ee5381fcd659d1e41ba12da6d8
Umar Hansa <umar.hansa@gmail.com>
2021-04-15 20:10:24 +0100

    update

---
+++ custom-audits/custom-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: 'lighthouse:default',
+
+  passes: [{
+    passName: 'defaultPass',
+    gatherers: [
+      'memory-gatherer',
+    ],
+  }],
+
+  audits: [
+    'memory-usage',
+    'javascript-object-count'
+  ],
+
+  categories: {
+    mysite: {
+      title: 'My Custom Audits',
+      description: 'Checks the memory usage and object count',
+      auditRefs: [
+        {id: 'memory-usage', weight: 1},
+        {id: 'javascript-object-count', weight: 1},
+      ],
+    },
+  },
+};
