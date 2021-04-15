d5db7c54c64eaa2d427dc083289ba5a0bc685977
Umar Hansa <umar.hansa@gmail.com>
2021-04-15 19:16:03 +0100

    fix

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const nunjucks = require('nunjucks');
 const app = express();
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+	autoescape: true,
+	express: app
 });
 
 app.set('port', process.env.PORT || 3000);
 
 // Home page
-app.get('/', function(req, res) {
-    res.render('index.html', {
-        page: 'home',
-        port: app.get('port')
-    });
+app.get('/', (request, response) => {
+	response.render('index.html', {
+		page: 'home',
+		port: app.get('port')
+	});
 });
 
 // Other example
-app.get('/example', function(req, res) {
-    res.render('example.html', {
-        page: 'example',
-        port: app.get('port')
-    });
+app.get('/example', (request, response) => {
+	response.render('example.html', {
+		page: 'example',
+		port: app.get('port')
+	});
 });
 
 // Kick start our server
-app.listen(app.get('port'), function() {
-    console.log('Server ready on port', app.get('port'));
+app.listen(app.get('port'), () => {
+	console.log('Server ready on port', app.get('port'));
 });
